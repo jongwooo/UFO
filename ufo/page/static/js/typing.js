30f8211964f965afb276c347746cce16309ef0a8
Jongwoo Han <han980817@gmail.com>
2019-06-30 03:20:12 +0900

    Added typeWriter func

--- ufo/page/static/js/typing.js
+++ ufo/page/static/js/typing.js
@@ -1,13 +1,11 @@
-var typing = document.getElementById('home_typing');
+let typing = document.getElementById('typing');
 
-var typewriter = new Typewriter(typing, {
-    loop: true
+let typewriter = new Typewriter(typing, {
+  loop: true
 });
 
-typewriter.typeString('우리 주변의 선택')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('우주선')
-    .pauseFor(2500)
-    .deleteAll()
-    .start();
+typewriter.typeString('우리 주변의 선택,')
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString('우주선')
+  .start();
